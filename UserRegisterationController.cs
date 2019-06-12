6c311ab637bb05105e44d0240c405eebdd87bbfa
Rawat Amardeep ArjunPrasad <47588491+Amardeep9@users.noreply.github.com>
2019-06-12 09:47:08 +0530

    Add files via upload

---
+++ UserRegisterationController.cs
@@ -0,0 +1,120 @@
+﻿using RegisterationForminMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegisterationForminMVC.Controllers
+{
+    public class UserRegisterationController : Controller
+    {
+
+
+        clsUserReg clsUser = new clsUserReg();
+
+        public ActionResult GetAllUser()
+        {
+            clsUserReg obj2 = new clsUserReg();
+            List<clsUserReg> lst = obj2.GetUsers();
+
+
+            return View(lst);
+        }
+
+
+
+        [HttpGet]
+        public ActionResult CreateRegForm()
+        {
+            clsUserReg obj = new clsUserReg();
+            obj.Department = GetDepartment();
+            return View(obj);
+        }
+
+        private List<SelectListItem> GetDepartment()
+        {
+            //Get all deprment 
+            clsDepartment objclsDepartment = new clsDepartment();
+            List<clsDepartment> lstDept = objclsDepartment.GetDept();
+
+            List<SelectListItem> itemslst = new List<SelectListItem>();
+
+            foreach (clsDepartment obj in lstDept)
+            {
+                SelectListItem item = new SelectListItem();
+                item.Value = Convert.ToString(obj.DeptID);
+                item.Text = obj.DeptName;
+                //  adding object to list 
+                itemslst.Add(item);
+            }
+
+            return itemslst;
+        }
+
+        [HttpPost]
+        public ActionResult CreateRegForm (string EmpName, string bday, string DeptID, string EmpAdress, string City, string EmpMob)
+        {
+            clsUserReg obj = new clsUserReg();
+
+            DateTime dtBday = Convert.ToDateTime(bday);
+            int intDeptID = Convert.ToInt32(DeptID);
+            double dblMob = Convert.ToDouble(EmpMob);
+
+            obj.AddUser(EmpName, dtBday, intDeptID, EmpAdress, City, dblMob);
+
+
+
+
+            obj.Department = GetDepartment();
+            ViewBag.message = "Registeration done successfully";
+            return View(obj);
+
+          //  return RedirectToActionPermanent("GetAllUser");
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            clsUserReg objnew = new clsUserReg();
+
+            objnew = objnew.GetEUserData(id);
+
+            return View("EditUser", objnew);
+        }
+
+        [HttpPost]
+
+
+        public ActionResult Edit(int id, clsUserReg objuser)
+        {
+            clsUserReg objnew = new clsUserReg();
+            objnew.UpdateUser(objuser);
+
+            //List<clsUserReg> lst = objnew.GetUsers();
+
+            //return View("GetUsers", lst);
+
+            return RedirectToActionPermanent("GetAllUser");
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+
+
+            clsUserReg clsUser3 = new clsUserReg();
+            clsUser3.DeleteUser(id);
+            List<clsUserReg> lst = clsUser3.GetUsers();
+            return View("GetAllUser", lst);
+        }
+
+
+
+
+
+    }
+}
---
+++ clsUserReg.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegisterationForminMVC.Models
+{
+    public class clsUserReg
+    {
+        string connectionString = @"Data Source=localhost\sqlexpress;Initial Catalog=Employee; Integrated Security=true;";
+
+        public string EmpID { get; set; }
+        public string EmpName { get; set; }
+
+        public string bday { get; set; }
+
+        public string EmpAdress { get; set; }
+
+        public string City { get; set; }
+
+        public string EmpMob { get; set; }
+
+        public string DeptName { get; set; }
+
+        public string DeptID { get; set; }
+        public List<SelectListItem> Department { get; set; }
+
+
+
+        public List<clsUserReg> GetUsers()
+        {
+
+            // object of list of type clsUserReg 
+            List<clsUserReg> lstUsers = new List<clsUserReg>();
+
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllUsers", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    clsUserReg obj1 = new clsUserReg();
+                    obj1.EmpName = Convert.ToString(rdr["EmpName"]);
+                    obj1.City = Convert.ToString(rdr["City"]);
+                    obj1.EmpAdress = Convert.ToString(rdr["EmpAddress"]);
+                    obj1.EmpMob = Convert.ToString(rdr["EmpMob"]);
+                    obj1.EmpID = Convert.ToString(rdr["EmpID"]);
+                    obj1.DeptName = Convert.ToString(rdr["DeptName"]);
+                    obj1.EmpID = Convert.ToString(rdr["EmpID"]);
+                    obj1.bday = Convert.ToString(rdr["bday"]);
+                    obj1.bday = Convert.ToDateTime(obj1.bday).ToString("dd/MM/yyyy");
+
+
+                    // end depart
+
+
+                    lstUsers.Add(obj1);
+
+
+                }
+
+                con.Close();
+            }
+            return lstUsers;
+        }
+
+
+        public clsUserReg GetEUserData(int id)
+        {
+            clsUserReg obj1 = new clsUserReg();
+
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sqlQuery = "SELECT * FROM tblUserReg WHERE EmpID= " + id;
+                SqlCommand cmd = new SqlCommand(sqlQuery, con);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    obj1.EmpName = Convert.ToString(rdr["EmpName"]);
+                    obj1.City = Convert.ToString(rdr["City"]);
+                    obj1.EmpAdress = Convert.ToString(rdr["EmpAddress"]);
+                    obj1.EmpMob = Convert.ToString(rdr["EmpMob"]);
+                    obj1.EmpID = Convert.ToString(rdr["EmpID"]);
+                    obj1.DeptID = Convert.ToString(rdr["DeptID"]);
+                    obj1.EmpID = Convert.ToString(rdr["EmpID"]);
+                    obj1.bday = Convert.ToString(rdr["bday"]);
+                    obj1.bday = Convert.ToDateTime(obj1.bday).ToString("dd/MM/yyyy");
+
+
+                    //Get all deprment 
+                    clsDepartment objclsDepartment = new clsDepartment();
+                    List<clsDepartment> lstDept = objclsDepartment.GetDept();
+
+                    List<SelectListItem> itemslst = new List<SelectListItem>();
+
+                    foreach (clsDepartment obj in lstDept)
+                    {
+                        SelectListItem item = new SelectListItem();
+                        item.Value = Convert.ToString(obj.DeptID);
+                        item.Text = obj.DeptName;
+                        //  adding object to list 
+                        itemslst.Add(item);
+                    }
+
+                    obj1.Department = itemslst;
+                    // end depart
+
+                }
+            }
+            return obj1;
+        }
+
+        public void UpdateUser(clsUserReg obj21)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spUpdateUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpID", obj21.EmpID);
+                cmd.Parameters.AddWithValue("@EmpName", obj21.EmpName);
+                cmd.Parameters.AddWithValue("@bday", obj21.bday);
+                cmd.Parameters.AddWithValue("@EmpAddress", obj21.EmpAdress);
+                cmd.Parameters.AddWithValue("@City", obj21.City);
+                cmd.Parameters.AddWithValue("@EmpMob", obj21.EmpMob);
+                cmd.Parameters.AddWithValue("@DeptID", obj21.DeptID);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+
+        public void AddUser(string EmpName, DateTime bday, int DeptID, string EmpAdress, string City, Double EmpMob)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpName", EmpName);
+                cmd.Parameters.AddWithValue("@bday", bday);
+                cmd.Parameters.AddWithValue("@DeptID", DeptID);
+                cmd.Parameters.AddWithValue("@EmpAdress", EmpAdress);
+                cmd.Parameters.AddWithValue("@City", City);
+                cmd.Parameters.AddWithValue("@EmpMob", EmpMob);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+        public void DeleteUser(int? id)
+        {
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spDeleteUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@EmpID", id);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+
+
+
+
+
+    }
+}
